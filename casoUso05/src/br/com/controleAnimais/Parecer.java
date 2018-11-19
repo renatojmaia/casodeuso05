80e9089b544525c4019900ca07d634808d11239b
26151596 <26151596@UNA009-L106-009.serlab.intra>
2018-11-19 20:53:10 -0300

    Parecer

--- casoUso05/src/br/com/controleAnimais/Parecer.java
+++ casoUso05/src/br/com/controleAnimais/Parecer.java
@@ -5,22 +5,28 @@ public class Parecer {
 	private int id_parecer;
 	private Docente docente;
 	private String parecer;
+	private int status;
 	
-	public Parecer(int id_parecer, Docente docente, String parecer) {
+	public Parecer(int id_parecer, Docente docente, String parecer, int status) {
 		super();
 		this.id_parecer = id_parecer;
 		this.docente = docente;
 		this.parecer = parecer;
+		this.status = status;
 	}
 
-	public Parecer(Docente docente, String parecer) {
+	public Parecer(Docente docente, String parecer, int status) {
 		super();
 		this.docente = docente;
 		this.parecer = parecer;
+		this.status = status;
 	}
 
 	public int getId_parecer() {
 		return id_parecer;
+	}
+	public int getStatus() {
+		return status;
 	}
 
 	public void setId_parecer(int id_parecer) {
@@ -41,6 +47,10 @@ public class Parecer {
 
 	public void setParecer(String parecer) {
 		this.parecer = parecer;
+	}
+	
+	public void setStatus(int status) {
+		this.status = status;
 	}
 	
 	
