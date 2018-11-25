44f7a259907bae1178a8dfcb258bfa817883c474
Renato Maia <Renato Maia@DESKTOP-C94C9CC>
2018-11-25 19:30:45 -0300

    Atualização do banco, identação dos codigos e comentar os códigos.

--- casoUso05/src/br/com/controleAnimais/Animal.java
+++ casoUso05/src/br/com/controleAnimais/Animal.java
@@ -1,15 +1,16 @@
 package br.com.controleAnimais;
 
+//Classe Animal
 public class Animal {
+	
+	//Declaração dos atributos
 	private int id_animal;
 	private String especie;
 	private String bioterio;
 	private String nome;
 	private String grupo;
 	
-	
-
-
+	//Construtor parametrizado
 	public Animal(int id_animal, String especie, String bioterio, String nome, String grupo) {
 		super();
 		this.especie = especie;
@@ -19,7 +20,7 @@ public class Animal {
 		this.id_animal = id_animal;
 	}
 	
-
+	//construtor parametrizado
 	public Animal(String especie, String bioterio, String nome, String grupo) {
 		super();
 		this.especie = especie;
@@ -28,47 +29,49 @@ public class Animal {
 		this.grupo = grupo;
 	}
 
-
+	//Metodos Especie
+	//GET
 	public String getEspecie() {
 		return especie;
 	}
 
-
+	//SET
 	public void setEspecie(String especie) {
 		this.especie = especie;
 	}
 
-
+	//Metodos Bioterio
+	//GET
 	public String getBioterio() {
 		return bioterio;
 	}
 
-
+	//SET
 	public void setBioterio(String bioterio) {
 		this.bioterio = bioterio;
 	}
 
-
+	//Metodo Nome
+	//GET
 	public String getNome() {
 		return nome;
 	}
 
-
+	//SET
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
+	//Metodos Grupo
+	//GET
 	public String getGrupo() {
 		return grupo;
 	}
 
-
+	//SET
 	public void setGrupo(String grupo) {
 		this.grupo = grupo;
 	}
 	
 	
-
-
 }
--- casoUso05/src/br/com/controleAnimais/Bioterio.java
+++ casoUso05/src/br/com/controleAnimais/Bioterio.java
@@ -1,11 +1,16 @@
 package br.com.controleAnimais;
 
+
+//Classe Bioterio
 public class Bioterio {
+	
+	//Declaração dos Atributos
 	private int id_bioterio;
 	private String nome;
 	private String local;
 	private String curso;
 	
+	//Contrutor Parametrizado
 	public Bioterio(String nome, String local, String curso) {
 		super();
 		this.nome = nome;
@@ -13,7 +18,7 @@ public class Bioterio {
 		this.curso = curso;
 	}
 
-	
+	//Construtor parametrizado
 	public Bioterio(int id_bioterio, String nome, String local, String curso) {
 		super();
 		this.id_bioterio = id_bioterio;
@@ -22,41 +27,45 @@ public class Bioterio {
 		this.curso = curso;
 	}
 
+	//Metodos bioterio
+	//GET
 	public int getId_bioterio() {
 		return id_bioterio;
 	}
-
+	//SET
 	public void setId_bioterio(int id_bioterio) {
 		this.id_bioterio = id_bioterio;
 	}
 
+	//Metodos Nome
+	//GET
 	public String getNome() {
 		return nome;
 	}
 
+	//SET
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+	//Metodos Local
+	//GET
 	public String getLocal() {
 		return local;
 	}
-
+	//SET
 	public void setLocal(String local) {
 		this.local = local;
 	}
 
+	//Metodos Curso
+	//GET
 	public String getCurso() {
 		return curso;
 	}
-
+	//SET
 	public void setCurso(String curso) {
 		this.curso = curso;
 	}
 	
-	
-	
-	
-	
-
 }
